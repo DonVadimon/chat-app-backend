b4ffa966441424404eb9e84ebf46c68a37a858bf
DonVadimon <provcydia@gmail.com>
2022-09-17 19:12:48 +0300

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 
@@ -11,7 +12,25 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.use(cookieParser());
-    app.enableCors({ credentials: true, origin: 'http://localhost:3001' });
+    app.enableCors({
+        credentials: true,
+        origin: [
+            'http://localhost:8100',
+            'https://chat-app-frontend-two.vercel.app',
+            'https://chat-app-frontend-git-master-antoffee.vercel.app',
+            'https://chat-app-frontend-antoffee.vercel.app',
+        ],
+    });
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Chat App Backend - SWAGGER')
+        .setDescription('chat app backend')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('swagger', app, document);
+
     await app.listen(process.env.PORT || process.env.APP_PORT);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { UserRoles } from '@prisma/client';
 
 import { RolesGuard } from '@/auth/guards/roles.guard';
@@ -7,12 +8,15 @@ import { CheckUsernameAvailableDto } from './dto/check-username-available.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
+import { ApiCheckUsernameAvailableResponse, ApiUserEntityResponse } from './users.swagger';
 import { UserReq } from './users.types';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @ApiOkResponse({ type: ApiCheckUsernameAvailableResponse })
     @Post('/check-username')
     async checkUsernameAvailable(@Body() dto: CheckUsernameAvailableDto) {
         const findedUser = await this.usersService.getByUsername(dto.username);
@@ -20,41 +24,48 @@ export class UsersController {
         return { available: !findedUser };
     }
 
+    @ApiOkResponse({ type: ApiUserEntityResponse })
     @Post()
     createUser(@Body() dto: CreateUserDto) {
         return this.usersService.createUser(dto);
     }
 
+    @ApiOkResponse({ type: ApiUserEntityResponse, isArray: true })
     @Get()
     @UseGuards(RolesGuard(UserRoles.ADMIN))
     getAllUsers() {
         return this.usersService.getAllUsers();
     }
 
+    @ApiOkResponse({ type: ApiUserEntityResponse })
     @Get('/self')
     @UseGuards(RolesGuard(UserRoles.ADMIN, UserRoles.REGULAR))
     getSelf(@Req() request: UserReq) {
         return this.usersService.getById(request.user.id);
     }
 
+    @ApiOkResponse({ type: ApiUserEntityResponse })
     @Get(':id')
     @UseGuards(RolesGuard(UserRoles.ADMIN))
     getById(@Param('id') id: number) {
         return this.usersService.getById(id);
     }
 
+    @ApiOkResponse({ type: ApiUserEntityResponse })
     @Patch('/self-update')
     @UseGuards(RolesGuard(UserRoles.ADMIN, UserRoles.REGULAR))
     updateSelf(@Req() request: UserReq, @Body() dto: UpdateUserDto) {
         return this.usersService.updateUser(request.user.id, dto);
     }
 
+    @ApiOkResponse({ type: ApiUserEntityResponse })
     @Patch(':id')
     @UseGuards(RolesGuard(UserRoles.ADMIN))
     updateUser(@Param('id') id: number, @Body() dto: UpdateUserDto) {
         return this.usersService.updateUser(id, dto);
     }
 
+    @ApiOkResponse({ type: ApiUserEntityResponse })
     @Delete(':id')
     @UseGuards(RolesGuard(UserRoles.ADMIN))
     deleteUser(@Param('id') id: number) {
